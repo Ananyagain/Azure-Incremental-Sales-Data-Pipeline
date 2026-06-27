63c65669dd07cb6ed9faf656c10e87f55cf703e4
ananya gain <57400399+Ananyagain@users.noreply.github.com>
2026-06-27 11:51:16 +0530

    Rename stored procedure to usp_update_dateholder

--- sql_scripts/storedprocedures/SP_scripts.sql
+++ sql_scripts/storedprocedures/SP_scripts.sql
@@ -1,4 +1,4 @@
-create procedure UpdateDateHolder
+create procedure usp_update_dateholder
 @mydate Date
 as 
 begin
@@ -7,3 +7,13 @@ update dateholder
 set lastdate = @mydate;
 commit 
 end;
+
+
+--copy incremental data from sql db to bronze layer (source -> query)
+-- select * from SalesOrders 
+-- where orderdate>='@{activity('lastDate').output.value[0].LastDate}'
+-- and 
+-- orderdate<='@{activity('maxDate').output.value[0].maxDate}'
+
+
+--@activity('maxDate').output.value[0].maxDate
